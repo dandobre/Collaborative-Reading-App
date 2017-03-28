df86234320afb2743a428fa68cfd3d79a4d71157
Student <Student@zpr-l0007074.local>
2017-03-28 08:54:45 -0400

    Added Log In View Controller

---
+++ Collaborative Reading App/SignInVC.swift
@@ -0,0 +1,13 @@
+//
+//  SignInVC.swift
+//  Collaborative Reading App
+//
+//  Created by Student on 2017-03-28.
+//  Copyright © 2017 Daniel Dobre. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+}
