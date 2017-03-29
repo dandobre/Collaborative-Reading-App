049923b286bad475bb2673a37c8ccf8925d873fb
Student <Student@zpr-l0007074.local>
2017-03-29 09:05:15 -0400

    Added Assets and fixed problems in new Side menu button

--- Collaborative Reading App/ViewController.swift
+++ Collaborative Reading App/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sideMenuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sideMenuButton.target = revealViewController()
+        sideMenuButton.action = #selector(SWRevealViewController.revealToggle(_:))
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
